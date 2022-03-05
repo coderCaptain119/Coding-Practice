a6ef27c9e0a58e1553e0296aebc6afb55725a23e
Dinesh Vaghela <98166348+coderCaptain119@users.noreply.github.com>
2022-03-05 15:29:09 +0530

    Initial commit

---
+++ DSA_Full_Course/Data_Structure/Stack_Using_Two_Queues.java
@@ -0,0 +1,33 @@
+package dsa.DataStructure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Stack_Using_Two_Queues {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+    int size;
+
+    Stack_Using_Two_Queues(){
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+        size = 0;
+    }
+    public void push(int data){
+        while(!q1.isEmpty()){
+            q2.add(q1.remove());
+        }
+        q1.add(data);
+        while (!q2.isEmpty()){
+            q1.add(q2.remove());
+        }
+        size++;
+    }
+    public void pop(){
+        q1.remove();
+        size--;
+    }
+    public int peek(){
+        return q1.peek();
+    }
+}
